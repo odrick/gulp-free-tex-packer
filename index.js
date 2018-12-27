2df2afcae6346ad4664fe844de380f1718ef4616
alexander.norinchak <norinchak@gmail.com>
2018-12-27 22:11:48 +0200

    Move to core module

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 let through = require("through2");
 let path = require("path");
-let Jimp = require("jimp");
-let getPackerByType = require("./packers/index").getPackerByType;
-let getExporterByType = require("./exporters/index").getExporterByType;
-let getFilterByType = require("./filters").getFilterByType;
-let FilesProcessor = require("./FilesProcessor");
-let appInfo = require('./package.json');
+let texturePacker = require("free-tex-packer-core");
 
 function fixPath(path) {
     return path.split("\\").join("/");
@@ -16,73 +11,17 @@ function getExtFromPath(path) {
 }
 
 function getErrorDescription(txt) {
-    return appInfo.name + ": " + txt;
+    return "Gulp free texture packer: " + txt;
 }
 
 const SUPPORTED_EXT = ["png", "jpg", "jpeg"];
 
 module.exports = function(options) {
-    options = options || {};
-    options = Object.assign({}, options);
-    
-    options.textureName = options.textureName === undefined ? "pack-result" : options.textureName;
-    options.width = options.width === undefined ? 2048 : options.width;
-    options.height = options.height === undefined ? 2048 : options.height;
-    options.fixedSize = options.fixedSize === undefined ? false : options.fixedSize;
-    options.padding = options.padding === undefined ? 0 : options.padding;
-    options.allowRotation = options.allowRotation === undefined ? true : options.allowRotation;
-    options.detectIdentical = options.detectIdentical === undefined ? true : options.detectIdentical;
-    options.allowTrim = options.allowTrim === undefined ? true : options.allowTrim;
-    options.removeFileExtension = options.removeFileExtension === undefined ? false : options.removeFileExtension;
-    options.prependFolderName = options.prependFolderName === undefined ? true : options.prependFolderName;
-    options.textureFormat = options.textureFormat === undefined ? "png" : options.textureFormat;
-    options.base64Export = options.base64Export === undefined ? false : options.base64Export;
-    options.scale = options.scale === undefined ? 1 : options.scale;
-    options.tinify = options.tinify === undefined ? false : options.tinify;
-    options.tinifyKey = options.tinifyKey === undefined ? "" : options.tinifyKey;
-    options.filter = options.filter === undefined ? "none" : options.filter;
-
-    if(!options.packer) options.packer = "MaxRectsBin";
-    if(!options.packerMethod) options.packerMethod = "BestShortSideFit";
-    if(!options.exporter) options.exporter = "JsonHash";
-
-    let packer = getPackerByType(options.packer);
-    if(!packer) {
-        throw new Error(getErrorDescription("Unknown packer " + options.packer));
-    }
-
-    let packerMethod = packer.getMethodByType(options.packerMethod);
-    if(!packerMethod) {
-        throw new Error(getErrorDescription("Unknown packer method " + options.packerMethod));
-    }
-
-    let exporter;
-    if(typeof options.exporter == "string") {
-        exporter = getExporterByType(options.exporter);
-    }
-    else {
-        exporter = options.exporter;
-    }
-    
-    if(!exporter) {
-        throw new Error(getErrorDescription("Unknown exporter " + options.exporter));
-    }
-    
-    let filter = getFilterByType(options.filter);
-    if(!filter) {
-        throw new Error(getErrorDescription("Unknown filter " + options.filter));
-    }
-
-    options.packer = packer;
-    options.packerMethod = packerMethod;
-    options.exporter = exporter;
-    options.filter = filter;
-
     let files = [];
     let firstFile = null;
 
     function bufferContents(file, enc, cb) {
-        if (file.isNull()) {
+		if (file.isNull()) {
             cb();
             return;
         }
@@ -100,45 +39,26 @@ module.exports = function(options) {
             return;
 		}
 		
-		Jimp.read(file.contents, (err, image) => {
-			if (err) {
-				console.error(getErrorDescription("Error reading " + file.relative));
-				cb();
-				return;
-			}
-			
-			image.name = fixPath(file.relative);
-			image._base64 = file.contents.toString("base64");
-			image.width = image.bitmap.width;
-			image.height = image.bitmap.height;
-			files.push({file: file, image: image});
-			cb();
-		});
+		files.push({path: fixPath(file.relative), contents: file.contents});
+		
+		cb();
     }
 
     function endStream(cb) {
-        if (!files.length) {
+		if (!files.length) {
             cb();
             return;
         }
-
-        let images = {};
-        for(let file of files) images[file.image.name] = file.image;
-
-        FilesProcessor.start(images, options, 
-            (res) => {
-                for(let item of res) {
-                    let file = firstFile.clone({contents: false});
-                    file.path = path.join(firstFile.base, item.name);
-                    file.contents = item.buffer;
-                    this.push(file);
-                }
-                cb();
-            },
-            (error) => {
-				console.error(getErrorDescription(error.description));
-                cb();
-            });
+		
+		texturePacker(files, options, (files) => {
+			for(let item of files) {
+				let file = firstFile.clone({contents: false});
+				file.path = path.join(firstFile.base, item.name);
+				file.contents = item.buffer;
+				this.push(file);
+			}
+			cb();
+		});
     }
 
     return through.obj(bufferContents, endStream);
